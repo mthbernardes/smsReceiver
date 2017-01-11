ad1b103b417af6b71b353be4afe8d34da4f89746
Matheus Bernardes <matheus.bernardes@pythonicos.com.br>
2017-01-11 01:00:43 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='smsReceiver',
-    version='1.2',
+    version='1.6',
     url='https://github.com/mthbernardes/smsReceiver',
     license='MIT License',
     author='Matheus Bernardes',
     author_email='mthbernardes@gmail.com',
     keywords='sms receive',
     description=u'Module to get online number to receive SMS',
-    packages=['smsReceiver'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['requests','lxml'],
 )
